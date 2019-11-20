bd651a7080b3797db350d2ed6705027a3c0b6138
Briana Bricher <bbricher@udallas.edu>
2019-11-20 07:51:07 -0600

    changed navigation to tabs on home page

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,20 @@
 import React, { Component } from 'react';
+import { Tab, Nav, Row, Col } from 'react-bootstrap';
+
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 
 //import logo from './logo.svg';
 import './App.css';
-import Nav from './Components/Nav/CoachToolbar/CoachToolBar';
-import Home from './Components/Home';
+// import Nav from './Components/Nav/CoachToolbar/CoachToolBar';
+// import Home from './Components/Home';
+import CoachHome from './pages/coach-side/CoachHome'
 import Messaging from '././pages/coach-side/Messaging';
 import Profile from '././pages/coach-side/Profile';
 import Training from '././pages/coach-side/Training'
 // import Toolbar from './Components/CoachToolBar/CoachToolBar';
 import Schedule from '././pages/coach-side/Schedule'
-import Weekly from './Components/Scheduling/Weekly';
+// import Weekly from './Components/Scheduling/Weekly';
 //import {Provider} from 'react-redux';
 //import store from './redux/store';
 
@@ -22,24 +25,47 @@ class App extends Component {
 
     return (
       <>
-      <Training />
-      <Weekly/>
-      <React.Fragment>
-        <Router>
-          <Switch>
-            <Nav />
-            
-            <Schedule /> 
-            <Route path="/" component = {Home} />         
-            <Route path="/Schedule" component={Schedule} />
-            <Route path="/Messaging" component={Messaging} />
-            <Route path="/Profile" component={Profile} />
-            <Route path="/Training" component={Training} />
-          </Switch>
-        </Router>
-
-
-      </React.Fragment>
+      <Tab.Container id="left-tabs-example" defaultActiveKey="first">
+        <Row>
+          <Nav variant="pills">
+            <Nav.Item>
+              <Nav.Link eventKey="home">Home</Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link eventKey="Schedule">Schedule</Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link eventKey="Profile">Profile</Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link eventKey="Training">Training</Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link eventKey="Messaging">Messaging</Nav.Link>
+            </Nav.Item>
+          </Nav>
+        </Row>  
+        <Row>  
+            <Tab.Content>
+              <Tab.Pane eventKey="home">
+                <CoachHome />
+              </Tab.Pane>
+              <Tab.Pane eventKey="Schedule">
+                <Schedule />
+              </Tab.Pane>
+              <Tab.Pane eventKey="Profile">
+                <Profile />
+              </Tab.Pane>
+              <Tab.Pane eventKey="Training">
+                <Training />
+              </Tab.Pane>
+              <Tab.Pane eventKey="Messaing">
+                <Messaging />
+              </Tab.Pane>
+            </Tab.Content>
+        </Row>          
+      </Tab.Container>
+      
       </>
     );
   }
--- client/src/pages/coach-side/CoachHome.js
+++ client/src/pages/coach-side/CoachHome.js
@@ -1,29 +1,20 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 // import './App.css';
-import Nav from './';
+// import Nav from './';
 // import MonthlyCalendar from './Components/MonthlyCalendar';
-import Messaging from './pages/coach-side/Messaging';
+// import Messaging from './pages/coach-side/Messaging';
 
 class App extends Component {
 
   render() {
 
     return (
-      <Router>
-        <>
-
-          <Nav />
-          
-
-          <Switch>
-          
-           <Route exact path="/Messaging" component= {Messaging}/>
-          </Switch>
-
-         
-        </>
-      </Router>
+      <>
+        <h1>
+          Hello World!
+        </h1>
+      </>
 
     )
   }
